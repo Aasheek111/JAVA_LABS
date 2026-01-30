4ed9a06f1897331b9b262e6223cab86fa263fdf8
Aashik Gautam <technicalaashik111@gmail.com>
2026-01-30 14:31:16 +0545

    network simple

--- Java/src/Network/client.java
+++ Java/src/Network/client.java
@@ -1,40 +1,44 @@
-
 import java.net.*;
-import java.util.Scanner;;
+import java.util.Scanner;
 import java.io.*;
 
 public class client {
 
     public static void main(String[] args) {
 
-        Socket socket=null;
-        InputStreamReader isr=null;
-        OutputStreamWriter osr=null;
-        BufferedReader br=null;
-        BufferedWriter bw= null;\
-        Scanner scanner=new Scanner(System.in);
-        try{
-            socket=new Socket("localhost",1234);
-            isr=new InputStreamReader(socket.getInputStream());
-            osr=new OutputStreamWriter(socket.getOutputStream());
-            br=new BufferedReader(isr);
-            bw=new BufferedWriter(osr);
+        Socket socket = null;
+        InputStreamReader isr = null;
+        OutputStreamWriter osr = null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            socket = new Socket("localhost", 1234);
+
+            isr = new InputStreamReader(socket.getInputStream());
+            osr = new OutputStreamWriter(socket.getOutputStream());
+
+            br = new BufferedReader(isr);
+            bw = new BufferedWriter(osr);
 
             while (true) {
 
-                String message=scanner.nextLine();      
+                String message = scanner.nextLine();
+
                 bw.write(message);
                 bw.newLine();
                 bw.flush();
 
+                if (message.equals("bye")) {
+                    break;
+                }
 
-                
+                System.out.println("Server = " + br.readLine());
             }
 
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
-
     }
-
 }
--- Java/src/Network/server.java
+++ Java/src/Network/server.java
@@ -1,4 +1,49 @@
 
+import java.net.*;
+import java.io.*;
+
 public class server {
-    
+
+    public static void main(String[] args) {
+
+        InputStreamReader ir = null;
+        OutputStreamWriter ow = null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        Socket socket = null;
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(1234);
+
+            socket = serverSocket.accept();
+            System.out.print("client connected");
+
+            ir = new InputStreamReader(socket.getInputStream());
+            br = new BufferedReader(ir);
+
+            ow = new OutputStreamWriter(socket.getOutputStream());
+            bw = new BufferedWriter(ow);
+
+            while (true) {
+
+                String message = br.readLine();
+                System.out.println("Client =" + message);
+
+                bw.write("Message recieved");
+                bw.newLine();
+                bw.flush();
+
+                if (message.equals("bye")) {
+                    break;
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
